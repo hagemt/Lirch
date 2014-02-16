61c826578a54477a28c399710379c8f87dec456f
Tor E Hagemann <hagemt@rpi.edu>
2014-02-16 06:42:14 -0800

    Preliminary adjustments to project layout (core)

--- core/plugin_loader.h
+++ core/plugin_loader.h
@@ -1,3 +1,16 @@
-#include "message_view.h"
+#ifndef __LIRCH_CORE_PLUGIN_LOADER_H__
+#define __LIRCH_CORE_PLUGIN_LOADER_H__
 
-bool load_plugin(const std::string &, const plugin_pipe &);
+#include <string>
+
+#include "message_view.h"
+//#include "lirch/core/message_view.h"
+
+//namespace lirch {
+
+extern bool
+load_plugin(const std::string &, const plugin_pipe &);
+
+//} // namespace lirch
+
+#endif // __LIRCH_CORE_PLUGIN_LOADER_H__
