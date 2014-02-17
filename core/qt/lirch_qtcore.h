a0df1b7fa37d3e957774d39716f633ddc9b0ffd7
Tor E Hagemann <hagemt@rpi.edu>
2014-02-16 21:41:44 -0800

    Simplify the Qt-core interaction; what a farce

--- core/qt/lirch_qtcore.h
+++ core/qt/lirch_qtcore.h
@@ -1,30 +1,68 @@
+#ifndef __LIRCH_CORE_QT__
+#define __LIRCH_CORE_QT__
+
+#include <iostream>
 #include <thread>
 #include <vector>
 
+#include <QCoreApplication>
 #include <QObject>
 
-//Yes, these have to go in a header file.  No, I don't know why.
-class core_waiter : public QObject
-{
-	Q_OBJECT
+#include "core/message.h"
+
+void run_core(const std::vector<message> &);
+
+//namespace lirch {
+//namespace core {
+
+// Can actually manage more than one client on single core?
+class lirch_core : public QObject {
+    Q_OBJECT;
+	std::thread *core_thread;
+    std::vector<message> messages;
 
 public:
-	core_waiter(std::thread &tt) : t(tt) {}
+    constexpr static int EXEC_FAILURE = -1;
+    lirch_core(const std::vector<message> &pp) : core_thread(nullptr) {
+		// FIXME(teh): could pass app and handle connect here?
+		for (const message &m : pp) {
+            //std::cerr << "[DEBUG] " << m << " (core invocation)" << std::endl;
+			messages.push_back(m);
+		}
+	}
+
+	virtual ~lirch_core() {
+		if (core_thread) {
+            //std::cerr << "[WARNING] core still in use (will detach)" << std::endl;
+			core_thread->detach();
+		}
+	}
+
+    int exec(QCoreApplication &app) {
+		if (core_thread) return EXEC_FAILURE;
+		core_thread = new std::thread([this, messages](){
+			run_core(messages);
+			// TODO(teh): check if app has quit?
+			this->quit();
+		});
+		return app.exec();
+	}
 
 public slots:
-	void onQuit() {t.join();}
-
-private:
-	std::thread &t;
-};
-
-class core_notifier : public QObject
-{
-	Q_OBJECT
-
-public:
-	void emitQuit() {emit quit();}
+	void quit() {
+		if (core_thread) {
+			emit aboutToQuit();
+			core_thread->join();
+			delete core_thread;
+			core_thread = nullptr;
+		}
+	}
 
 signals:
-	void quit();
+	void aboutToQuit();
 };
+
+//} // namespace core
+//} // namespace lirch
+
+#endif // __LIRCH_CORE_QT__
